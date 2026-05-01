08dbaab0bc07e0006df08eb17252e3a01a658ea3
1129louie <chabitsheena29@gmail.com>
2026-05-01 23:04:32 +0800

    Implement diary entry creation and saving
    Add functionality to create and write to a diary file.

--- DIARY.py
+++ DIARY.py
@@ -1,1 +1,14 @@
+# Student A - Create and Write
 
+try:
+    file = open("diary.txt", "x")
+    file.close()
+except:
+    print("File already exists")
+
+file = open("diary.txt", "w")
+entry = input("Enter your first diary entry: ")
+file.write(entry + "\n")
+file.close()
+
+print("Entry saved!")
