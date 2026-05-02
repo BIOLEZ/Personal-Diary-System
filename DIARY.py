526706268c353b85725a485ca92a8264ef3fb351
1129louie <chabitsheena29@gmail.com>
2026-05-02 19:17:19 +0800

    Student A: Create and write file

--- DIARY.py
+++ DIARY.py
@@ -12,3 +12,14 @@ file.write(entry + "\n")
 file.close()
 
 print("Entry saved!")
+
+# Student B - Append
+
+file = open("diary.txt", "a")
+
+entry = input("Add another diary entry: ")
+file.write(entry + "\n")
+
+file.close()
+
+print("Entry appended!")
